985e4f2fd85983c6f091931be731d9f7a38335d4
pknaggs <pknaggs>
2014-09-28 14:50:34 +0000

    2014.4 Updates

--- make.bat
+++ make.bat
@@ -6,7 +6,6 @@ if x%1 == xdvi   goto dvi
 if x%1 == xps    goto ps
 if x%1 == xpdf   goto process
 if x%1 == xclean goto clean
-if x%1 == xhtml  goto dvi
 if x%1 == xzip   goto zip
 
 rem Usage
@@ -24,9 +23,7 @@ goto process
 :process
 title 1/5: Extract support files
 %latex% \scrollmode\input forth.tex
-rem perl sort.pl < forth.wrd > forth.wds
-
-if x%1 == xhtml goto html
+perl sort.pl < forth.wrd > forth.wds
 
 title 2/5: Create Labels
 %latex% forth
@@ -44,11 +41,6 @@ if not x%1 == xps goto end
 if not exist forth.dvi goto dvi
 
 dvips -K -t A4 forth
-goto end
-
-:html
-rem Build the html version
-htlatex forth
 goto end
 
 :zip
@@ -78,21 +70,6 @@ rem forth.ps	PostScript Final output
 rem forth.dvi	LaTeX DVI output
 
 goto end
-
-rem Clean HTML files
-rem forth.4ct	ToC postfix
-rem forth.4tc	ToC prefix
-rem forth.idv	Fonts extract
-rem forth.lg	tex4th log file
-rem forth.xref	tex4th Cross references
-rem forth*.tmp
-
-for %%x in (4ct 4tc idv lg tmp xref) do if exist forth.%%x del forth.%%x
-
-rem Keep the following:
-rem forth*.png	Graphics files
-rem *.html	HTML files
-rem *.css	HTML Style Sheets
 
 :end
 title Command prompt
